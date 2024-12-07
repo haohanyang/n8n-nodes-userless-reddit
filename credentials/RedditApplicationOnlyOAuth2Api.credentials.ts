ae89113fef7c96e012615ec09686cba85c747d77
Haohan Yang <yanghaohanmail@gmail.com>
2024-12-07 09:24:22 +0000

    Add reddit app only oauth2 credentials

---
+++ credentials/RedditApplicationOnlyOAuth2Api.credentials.ts
@@ -0,0 +1,86 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestHelper,
+	INodeProperties,
+} from 'n8n-workflow';
+
+// https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
+
+export class RedditApplicationOnlyOAuth2Api implements ICredentialType {
+	name = 'redditOAuth2ApplicationOnlyApi';
+	displayName = 'Reddit Application Only OAuth2 API';
+	// documentationUrl = 'userless-reddit';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Session Token',
+			name: 'sessionToken',
+			type: 'hidden',
+			typeOptions: {
+				expirable: true,
+				password: true,
+			},
+			default: '',
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+			required: true,
+		},
+	];
+
+	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+		const { access_token } = (await this.helpers.httpRequest({
+			method: 'POST',
+			url: 'https://www.reddit.com/api/v1/access_token',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'User-Agent': 'n8n-nodes-userless-reddit',
+			},
+			auth: {
+				username: credentials.clientId as string,
+				password: credentials.clientSecret as string,
+			},
+			body: 'grant_type=client_credentials',
+		})) as { access_token: string };
+
+		return {
+			sessionToken: access_token,
+		};
+	}
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.sessionToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://oauth.reddit.com',
+			url: '/api/v1/me',
+			headers: {
+				'Content-Type': 'application/json',
+				'User-Agent': 'n8n-nodes-userless-reddit',
+			},
+		},
+	};
+}
